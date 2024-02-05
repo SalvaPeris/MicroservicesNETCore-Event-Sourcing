541ff5a34447ba9d742b8018b0813e033dd4d174
speris <speris@grupokh.com>
2024-02-05 16:26:49 +0100

    Registered all command handlers.

--- Microservices.Posts.Commands.Api/Program.cs
+++ Microservices.Posts.Commands.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microservices.Posts.Commands.Api.Commands;
 using Microservices.Posts.Commands.Domain.Aggregates;
 using Microservices.Posts.Commands.Infrastructure.Config;
+using Microservices.Posts.Commands.Infrastructure.Dispatchers;
 using Microservices.Posts.Commands.Infrastructure.Handlers;
 using Microservices.Posts.Commands.Infrastructure.Repositories;
 using Microservices.Posts.Commands.Infrastructure.Stores;
@@ -15,6 +16,20 @@ builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
 builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+// Command handler methods
+var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispatcher = new CommandDispatcher();
+
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
+
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
