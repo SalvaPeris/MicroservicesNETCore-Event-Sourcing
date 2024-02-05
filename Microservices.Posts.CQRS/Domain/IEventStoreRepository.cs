ef1d2311f908d11479b81bcbc700e7f2047371b8
speris <speris@grupokh.com>
2024-02-05 14:09:21 +0100

    Added EventModel, EventStoreRepository and MongoDB configuration.

---
+++ Microservices.Posts.CQRS/Domain/IEventStoreRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microservices.Posts.CQRS.Events;
+
+namespace Microservices.Posts.CQRS.Domain
+{
+    public interface IEventStoreRepository
+    {
+        Task SaveAsync(EventModel @event);
+        Task<IEnumerable<EventModel>> FindByAggregateIdAsync(Guid aggregateId);
+    }
+}
