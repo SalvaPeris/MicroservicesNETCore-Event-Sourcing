28b3d863f872a1df023ec5a1b2283739d186180d
speris <speris@grupokh.com>
2024-02-05 16:21:43 +0100

    Added CommandHandler.

---
+++ Microservices.Posts.Commands.Api/Commands/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Microservices.Posts.Commands.Api.Commands
+{
+    public interface ICommandHandler
+    {
+        Task HandleAsync(NewPostCommand comand);
+        Task HandleAsync(EditMessageCommand comand);
+        Task HandleAsync(LikePostCommand comand);
+        Task HandleAsync(AddCommentCommand comand);
+        Task HandleAsync(EditCommentCommand comand);
+        Task HandleAsync(RemoveCommentCommand comand);
+        Task HandleAsync(DeletePostCommand comand);
+    }
+}
--- Microservices.Posts.Commands.Api/Program.cs
+++ Microservices.Posts.Commands.Api/Program.cs
@@ -1,7 +1,11 @@
+using Microservices.Posts.Commands.Api.Commands;
+using Microservices.Posts.Commands.Domain.Aggregates;
 using Microservices.Posts.Commands.Infrastructure.Config;
+using Microservices.Posts.Commands.Infrastructure.Handlers;
 using Microservices.Posts.Commands.Infrastructure.Repositories;
 using Microservices.Posts.Commands.Infrastructure.Stores;
 using Microservices.Posts.CQRS.Domain;
+using Microservices.Posts.CQRS.Handlers;
 using Microservices.Posts.CQRS.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
