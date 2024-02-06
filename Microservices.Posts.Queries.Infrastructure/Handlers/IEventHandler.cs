0bee228642b05516719c66541a2f0fffeb5d0a2b
speris <speris@grupokh.com>
2024-02-06 13:33:12 +0100

    Added event handler.

---
+++ Microservices.Posts.Queries.Infrastructure/Handlers/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microservices.Posts.Common.Events;
+
+namespace Microservices.Posts.Queries.Infrastructure.Handlers
+{
+    public interface IEventHandler
+    {
+        Task On(PostCreatedEvent @event);
+        Task On(MessageUpdatedEvent @event);
+        Task On(PostLikedEvent @event);
+        Task On(CommentAddedEvent @event);
+        Task On(CommentUpdatedEvent @event);
+        Task On(CommentRemovedEvent @event);
+        Task On(PostRemovedEvent @event);
+
+    }
+}
