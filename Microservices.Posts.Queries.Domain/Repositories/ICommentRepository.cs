e201cada21b7f45df135535f8e69d619f356e0db
speris <speris@grupokh.com>
2024-02-06 10:27:54 +0100

    Added IPostRepository and ICommentRepository.

---
+++ Microservices.Posts.Queries.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microservices.Posts.Queries.Domain.Entities;
+
+namespace Microservices.Posts.Queries.Domain.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task CreateAsync(CommentEntity comment);
+        Task UpdateAsync(CommentEntity comment);
+        Task<CommentEntity> GetByIdAsync(Guid commentId);
+        Task DeleteAsync(Guid commentId);
+    }
+}
---
+++ Microservices.Posts.Queries.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microservices.Posts.Queries.Domain.Entities;
+
+namespace Microservices.Posts.Queries.Domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Task CreateAsync(PostEntity post);
+        Task UpdateAsync(PostEntity post);
+        Task DeleteAsync(Guid postId);
+
+        Task<PostEntity> GetByIdAsync(Guid postId);
+        Task<List<PostEntity>> ListAllAsync();
+        Task<List<PostEntity>> ListByAuhtorAsync(string author);
+        Task<List<PostEntity>> ListWithLikesAsync(int numberOfLikes);
+        Task<List<PostEntity>> ListWithCommentsAsync();
+    }
+}
